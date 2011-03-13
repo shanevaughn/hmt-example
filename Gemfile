40d04e5947fb78e4b1c0b8f1010ab8af9bf5f866
shane vaughn <shane@zindelo.com>
2011-03-13 10:22:28 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,22 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.5'
+gem 'sqlite3'
+
+
+group :development do
+  gem 'rspec-rails'
+  gem 'autotest-rails'
+  gem 'autotest-rails-pure'
+  gem 'autotest-growl'
+  gem 'autotest-fsevent'
+  gem 'annotate-models'
+  gem 'webrat'
+end
+
+group :test do
+  gem 'rspec'
+  gem 'factory_girl_rails'
+  gem 'mocha'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HmtExample::Application.config.secret_token = '70fe9f7dab0ed7dddabb76452d1431d33f69127289b2deb9a75d91d56b449e2cb44b84ee64b75c4bc67fd6592957b63a526ee740e0f2db970167feb6848c631f'
